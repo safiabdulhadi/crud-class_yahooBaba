f4abde7e1c326962955f382dc0cb6e8e660d7698
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2023-01-16 19:08:56 +0100

    creating function and variables

---
+++ database.php
@@ -0,0 +1,33 @@
+<?php
+class Database{
+    private $db_host = 'localhost';
+    private $db_user = 'root';
+    private $db_pass = 'Safi1994?';
+    private $db_name = 'testing';
+
+    public function __construct(){
+
+    }
+    // Function to insert into database
+    public function insert(){
+
+    }
+    // Function to update row in database
+    public function update(){
+
+    }
+    // Function to delete table or row(s) from database
+    public function delete(){
+
+    }
+    // Function to SELECT From database
+    public function select(){
+
+    }
+    // Close connection
+    public function __destruct(){
+
+    }
+} // Class close
+
+?>
